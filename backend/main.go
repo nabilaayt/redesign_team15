c0df97130b749a2fa5234789327b4082cef673ab
mauinGit <maulanaadiatma3133@gmail.com>
2025-08-17 21:58:19 +0700

    feat: use CORS

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,7 @@ import (
 	"team15gdg/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -14,8 +15,13 @@ func main() {
 	database.DBMigrate()
 
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173", // asal request frontend
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 	app.Static("/assets", "./assets")
-	
+
 	routes.MainRoutes(app)
 
 	app.Listen(":8080")
